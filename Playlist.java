b198966a5f8d7074b7ffbd05d6c6784bdbd78a77
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-24 16:45:19 -0400

    added method to remove a song and reverse playlist

--- Playlist.java
+++ Playlist.java
@@ -6,15 +6,6 @@ class Playlist {
     // shuffle playlist method
     public static void shufflePlaylist() {
         // ArrayList of strings
-            // not dry
-                // ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-                // songs for playlist
-                // desertIslandPlaylist.add("Through The Late Night");
-                // desertIslandPlaylist.add("Carousel");
-                // desertIslandPlaylist.add("Never Catch Me");
-                // desertIslandPlaylist.add("I Can Tell");
-                // desertIslandPlaylist.add("Motorcycle Patches");
-                // desertIslandPlaylist.add("Skeletons");
         List<String> desertIslandPlaylist = Arrays.asList("Through The Late Night", "Carousel", "Never Catch Me", "I Can Tell", "Motorcycle Patches", "Skeletons");
         // print playlist to console
         System.out.println("Original playlist: " + desertIslandPlaylist);
@@ -26,9 +17,37 @@ class Playlist {
         System.out.println("Shuffled playlist: " + desertIslandPlaylist);
     }
     // reverse playlist method
-
-    // create main method
+    public static void reversePlaylist() {
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+        // songs for playlist
+        desertIslandPlaylist.add("Through The Late Night");
+        desertIslandPlaylist.add("Carousel");
+        desertIslandPlaylist.add("Never Catch Me");
+        desertIslandPlaylist.add("I Can Tell");
+        desertIslandPlaylist.add("Motorcycle Patches");
+        desertIslandPlaylist.add("Skeletons");        
+        // remove a song
+        desertIslandPlaylist.remove("Skeletons");
+        // print updated size and playlist
+        System.out.println(desertIslandPlaylist.size());
+        System.out.println(desertIslandPlaylist);
+        // variables to store song index
+        int a = desertIslandPlaylist.indexOf("Through The Late Night");
+        int b = desertIslandPlaylist.indexOf("Carousel");       
+        // variable to store temporary string
+        String tempA = "Through The Late Night";
+        // change order of playlist
+        desertIslandPlaylist.set(a, "Carousel");
+        desertIslandPlaylist.set(b, tempA);
+        // print updated playlist to console
+        System.out.println("Playlist in order: " + desertIslandPlaylist);
+        // reverse playlist
+        Collections.reverse(desertIslandPlaylist);
+        System.out.println("Reversed playlist: " + desertIslandPlaylist); 
+    }
+    // main method
     public static void main(String[] args) {
         shufflePlaylist();
+        reversePlaylist();
     }
 }
