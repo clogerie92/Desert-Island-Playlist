1b2107a2fc725c7b96ae273a14d6bd5c4e11e0a0
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-24 10:58:57 -0400

    added code to remove a song from the playlist and print the updated playlist to the console.

--- Playlist.java
+++ Playlist.java
@@ -17,8 +17,15 @@ class Playlist {
         System.out.println(desertIslandPlaylist);
         // print size of playlist to console
         System.out.println(desertIslandPlaylist.size());
-        
-
+        // remove a song
+        desertIslandPlaylist.remove("Skeletons");
+        // print updated size and playlist
+        System.out.println(desertIslandPlaylist.size());
+        System.out.println(desertIslandPlaylist);
+        // change order of playlist
+        // variables to store song index
+        int a = desertIslandPlaylist.indexOf("Through The Late Night");
+        int b = 
 
     }
 }
