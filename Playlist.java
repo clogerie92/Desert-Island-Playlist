e2ad288f959eb8401f6062f1aa01189b75550c9a
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-24 10:42:34 -0400

    created ArrayList of strings for songs in playlist

--- Playlist.java
+++ Playlist.java
@@ -1,7 +1,11 @@
+// import ArrayList
 import java.util.ArrayList;
-
+// create Playlist class
 class Playlist {
+    // create main method
     public static void main(String[] args) {
+        // ArrayList of strings
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
         
     }
 }
